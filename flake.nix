607ebd614ea9b70ef5ee044df214ed21963af5b5
Sarah Schulte <git@sarah.engineer>
2026-07-29 19:28:53 -0400

    hm

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Static ARM32 cross-compilation flake";
+  description = "Static ARMv6 glibc cross-compilation flake for Kobo i.MX35";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -7,30 +7,34 @@
 
   outputs = { self, nixpkgs }:
     let
-      # Replace with your host system if different (e.g., "aarch64-linux")
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in {
       packages = forAllSystems (system:
         let
-          # Import nixpkgs for your host system, targeting static armv6l cross-compilation
           pkgs = import nixpkgs {
             inherit system;
             crossSystem = {
-              config = "armv6l-unknown-linux-gnueabihf"; # armv6 little endian hard-float
+              config = "armv6l-unknown-linux-gnueabi";
+              
+              # GCC expects "armv6" here, which covers the 6TEJ instruction set
+              gcc.arch = "armv6"; 
+              gcc.fpu = "vfp"; 
+              gcc.float = "soft"; # Force soft-float ABI layout matching gnueabi
+              
+              # Align internal nixpkgs layouts with the legacy target platform
+              platform = nixpkgs.lib.systems.platforms.raspberrypi;
             };
           };
-
-          # Use the static package set from the cross-compiler
-          staticPkgs = pkgs.pkgsStatic;
         in {
-          default = staticPkgs.stdenv.mkDerivation {
-            pname = "hello-arm32-static";
+          default = pkgs.stdenv.mkDerivation {
+            pname = "hello-kobo-armv6-static";
             version = "1.0.0";
             src = ./.;
 
-            # Explicitly force static flags during compilation
+            # Leverage the cross-compiled static glibc layer
+            buildInputs = [ pkgs.glibc.static ];
+
             buildPhase = ''
               $CC -static -O2 main.c -o hello
             '';
@@ -46,10 +50,16 @@
         let
           pkgs = import nixpkgs {
             inherit system;
-            crossSystem = { config = "armv6l-unknown-linux-gnueabihf"; };
+            crossSystem = { 
+              config = "armv6l-unknown-linux-gnueabi";
+              gcc.arch = "armv6";
+              gcc.fpu = "vfp";
+              gcc.float = "soft";
+              platform = nixpkgs.lib.systems.platforms.raspberrypi;
+            };
           };
         in {
-          default = pkgs.pkgsStatic.mkShell {
+          default = pkgs.mkShell {
             inputsFrom = [ self.packages.${system}.default ];
           };
         });
