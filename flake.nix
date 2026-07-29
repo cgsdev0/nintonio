0a3403c1aae3dfbb393ab49c9ea0ee669a1dc24f
Sarah Schulte <git@sarah.engineer>
2026-07-29 19:44:48 -0400

    argh

--- flake.nix
+++ flake.nix
@@ -1,68 +1,40 @@
 {
   description = "Static ARMv6 Musl cross-compilation flake for legacy Kobo (Kernel 2.6)";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      mkPkgs = system: import nixpkgs {
+        inherit system;
+        crossSystem = {
+          config = "armv6l-unknown-linux-musleabi"; # musleabi -> musl libc
+          gcc.arch = "armv6j";
+          gcc.fpu = "vfp";
+          gcc.float-abi = "softfp";
+        };
+      };
     in {
       packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            crossSystem = {
-              # 1. Must use soft-float gnueabi to prevent ABI mismatch crashes
-              config = "armv6l-unknown-linux-gnueabi";
-              
-              # 2. Match your 6TEJ processor exactly 
-              gcc.arch = "armv6"; 
-              gcc.fpu = "vfp"; 
-              gcc.float = "soft"; 
-
-              platform = nixpkgs.lib.systems.platforms.raspberrypi;
-            };
-          };
-
-          # 3. Swap Glibc out for Musl (pkgsStatic), which tolerates Linux 2.6 kernels
-          staticPkgs = pkgs.pkgsStatic;
+        let staticPkgs = (mkPkgs system).pkgsStatic;
         in {
           default = staticPkgs.stdenv.mkDerivation {
             pname = "hello-kobo-legacy-static";
             version = "1.0.0";
             src = ./.;
-
             buildPhase = ''
               $CC -static -O2 main.c -o hello
             '';
-
             installPhase = ''
               mkdir -p $out/bin
               cp hello $out/bin/
             '';
           };
         });
-
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            crossSystem = { 
-              config = "armv6l-unknown-linux-gnueabi";
-              gcc.arch = "armv6";
-              gcc.fpu = "vfp";
-              gcc.float = "soft";
-              platform = nixpkgs.lib.systems.platforms.raspberrypi;
-            };
-          };
-        in {
-          default = pkgs.pkgsStatic.mkShell {
-            inputsFrom = [ self.packages.${system}.default ];
-          };
-        });
+      devShells = forAllSystems (system: {
+        default = (mkPkgs system).mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+        };
+      });
     };
 }
-
