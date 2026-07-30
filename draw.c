dbb7ab6c4ae86edefdf1ac0f29b3e102c0c227ab
Sarah Schulte <git@sarah.engineer>
2026-07-29 23:03:27 -0400

    bad apple maybe

--- draw.c
+++ draw.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
@@ -10,39 +11,69 @@
 #define FBIO_WAIT1    0x4528
 #define FBIO_WAIT2    0x4529
 
-#define WIDTH  600
-#define HEIGHT 800
+#define FB_W   600
+#define FB_H   800
+#define IMG_W  360
+#define IMG_H  480
+#define BMPROW ((IMG_W * 3 + 3) & ~3)
 
 typedef char u8;
 typedef unsigned int u32;
+
+static int read_full(int fd, void *p, size_t n) {
+    unsigned char *q = p;
+    while (n) {
+        ssize_t r = read(fd, q, n);
+        if (r < 0) { if (errno == EINTR) continue; return -1; }
+        if (r == 0) return -1;                 /* truncated file */
+        q += r; n -= r;
+    }
+    return 0;
+}
+
+
 int main() {
     int fb = open("/dev/fb0", O_RDWR);
 
-struct upd {                              // 24-byte header, data follows inline
-    u32 x, y, w, h;
-    u32 mode_a, mode_b;
-    u8  data[600*800];
-} *buf = malloc(480024);                  // 0x75318 = 24 + 480000
-memset(buf->data, 0, 480000);             // 0x75300
+		for (int i = 1; i <= 659; i++) {
+		char * fname;
+		sprintf(fname, "%03d.bmp", i);
+    int bmp = open(fname, O_RDONLY);
+		lseek(bmp, 138, SEEK_SET);
+		struct upd {                              // 24-byte header, data follows inline
+				u32 x, y, w, h;
+				u32 mode_a, mode_b;
+				u8  data[FB_W*FB_H];
+		} *buf = malloc(sizeof(struct upd));                  // 0x75318 = 24 + 480000
+		memset(buf->data, 0, FB_W*FB_H);             // 0x75300
 
-buf->x = 0; buf->y = 0; buf->w = 600; buf->h = 800;
-buf->mode_a = 3; buf->mode_b = 1;
-ioctl(fb, 0x4702, buf);                   // blank screen w/ the zeroed payload
-ioctl(fb, 0x4528, 0);
-ioctl(fb, 0x4529, 0);
+		buf->x = 0; buf->y = 0; buf->w = FB_W; buf->h = FB_H;
+		buf->mode_a = 3; buf->mode_b = 1;
+		ioctl(fb, 0x4702, buf);                   // blank screen w/ the zeroed payload
+		ioctl(fb, 0x4528, 0);
+		ioctl(fb, 0x4529, 0);
 
-for (int n = 0; n != 480000; ) {              // rsb #479232 + add #768 == 480000 - n
-    int r = read(0, buf->data + n, 480000 - n);   // fd 0 == stdin
-    n += r;
-    if (r <= 0) return 0;                 // short input: bail, exit code 0
-}
+		unsigned char row[BMPROW];
+		const int x0 = (FB_W - IMG_W) / 2;             /* 120 */
+		const int y0 = (FB_H - IMG_H) / 2;             /* 160 */
 
-buf->x = 0; buf->y = 0; buf->w = 600; buf->h = 800;
-buf->mode_a = 2; buf->mode_b = 2;
-ioctl(fb, 0x4539, buf);                   // the actual draw
-ioctl(fb, 0x4528, 0); ioctl(fb, 0x4529, 0);
-ioctl(fb, 0x4528, 0); ioctl(fb, 0x4529, 0);
+		for (int i = 0; i < IMG_H; i++) {
+				if (read_full(bmp, row, BMPROW) < 0) return -1;
 
-free(buf); sleep(1); close(fb);
-return 1;
+				int y = y0 + (IMG_H - 1 - i);              /* BMP rows are bottom-up */
+				unsigned char *dst = buf->data + y * FB_W + x0;
+
+				for (int x = 0; x < IMG_W; x++)
+						dst[x] = row[x * 3 + 2];               /* +2 = red */
+		}
+
+		buf->x = 0; buf->y = 0; buf->w = FB_W; buf->h = FB_H;
+		buf->mode_a = 2; buf->mode_b = 2;
+		ioctl(fb, 0x4539, buf);                   // the actual draw
+		ioctl(fb, 0x4528, 0); ioctl(fb, 0x4529, 0);
+		ioctl(fb, 0x4528, 0); ioctl(fb, 0x4529, 0);
+
+		free(buf); }
+		sleep(1); close(fb);
+		return 0;
 }
