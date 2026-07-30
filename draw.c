05f2bf48f3f276f433cc5187df6b2d2650c7d66a
Sarah Schulte <git@sarah.engineer>
2026-07-29 22:03:01 -0400

    whee

--- draw.c
+++ draw.c
@@ -13,57 +13,35 @@
 #define WIDTH  600
 #define HEIGHT 800
 
-struct mxcfb_rect {
-    unsigned int top;
-    unsigned int left;
+struct ioctl_command {
+    unsigned int x;
+    unsigned int y;
     unsigned int width;
     unsigned int height;
-};
-
-struct mxcfb_update_data {
-    struct mxcfb_rect update_region;
-    unsigned int waveform_mode;
-    unsigned int update_mode;
-    unsigned int update_marker;
-    int temperature;
+    unsigned char buf[HEIGHT * WIDTH];
 };
 
 int main() {
-    int fd = open("/dev/fb0", O_RDWR);
-    if (fd < 0) { perror("open"); return 1; }
-
-    // Map the framebuffer memory and fill it with a test pattern
-    size_t fb_size = WIDTH * HEIGHT; // 1 byte per pixel, adjust if wrong
-    unsigned char *fb = mmap(NULL, fb_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (fb == MAP_FAILED) { perror("mmap"); close(fd); return 1; }
-
     // Simple test: fill with a mid-gray checkerboard
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-                fb[y * WIDTH + x] = 0xFF;
+            fb[y * WIDTH + x] = ((x / 20 + y / 20) % 2) ? 0x00 : 0xFF;
         }
     }
 
-    struct mxcfb_update_data args = {
-        .update_region = {
-            .top = 0,
-            .left = 0,
-            .width = 600,
-            .height = 800,
-        },
-        .waveform_mode = 3,
-        .update_mode = 1,
-        .update_marker = 0,
-        .temperature = 25,
-    };
+    struct ioctl_command *cmd = malloc(sizeof(*cmd));
 
-    msync(fb, fb_size, MS_SYNC);
+    cmd->x = 0;
+    cmd->y = 0;
+    cmd->width = 600;
+    cmd->height = 800;
+    memset(cmd->buf, 0xff, sizeof(cmd->buf));
 
-    if (ioctl(fd, FBIO_UPDATE, &args) < 0) perror("update ioctl");
+    if (ioctl(fd, FBIO_UPDATE, cmd) < 0) perror("update ioctl");
     if (ioctl(fd, FBIO_WAIT1, 0) < 0) perror("wait1 ioctl");
     if (ioctl(fd, FBIO_WAIT2, 0) < 0) perror("wait2 ioctl");
 
-    munmap(fb, fb_size);
+    free(cmd);
     close(fd);
     return 0;
 }
